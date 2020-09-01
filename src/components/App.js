9004392d7f3ca9c740ea7834b9cec8492f3d256a
Simon K. Bjørnøy <simon.bjornoy@gmail.com>
2020-09-01 16:34:24 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-black">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
